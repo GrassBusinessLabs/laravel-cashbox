2aba75a0d10e046a011b7b9be94361be191f9d8d
amaneshi <26366776+amaneshi@users.noreply.github.com>
2020-10-08 16:16:24 +0300

    Big bang
    Add cashbox migration and model
    Remove unused migrations
    Add package for additional meta generation

---
+++ database/factories/CashboxFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cashbox;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CashboxFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Cashbox::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
